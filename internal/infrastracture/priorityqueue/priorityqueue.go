9c9b4c1c2ea4a386b6c1330d66380feb2eaff8b5
FoxFurry <isacartur@gmail.com>
2021-10-05 21:25:28 +0000

    Localized priority queue for order rather than interfaces

--- internal/infrastracture/priorityqueue/priorityqueue.go
+++ internal/infrastracture/priorityqueue/priorityqueue.go
@@ -13,17 +13,6 @@ type PriorityQueue []*Item
 
 func (pq PriorityQueue) Len() int { return len(pq) }
 
-func (pq PriorityQueue) Less(i, j int) bool {
-	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
-	return pq[i].Priority > pq[j].Priority
-}
-
-func (pq PriorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
 func (pq *PriorityQueue) Push(x dto.Order) {
 	n := len(*pq)
 	item := &Item{
