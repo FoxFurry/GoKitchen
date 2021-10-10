c5276aba08cd58c1744752bf4dcd56ea3ed85f7c
FoxFurry <isacartur@gmail.com>
2021-10-10 21:27:59 +0000

    Added gateway layer

--- internal/service/supervisor/gateway/distribution.go
+++ internal/service/supervisor/gateway/distribution.go
@@ -1,1 +1,23 @@
 package gateway
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/foxfurry/go_kitchen/internal/domain/dto"
+	"github.com/spf13/viper"
+	"log"
+	"net/http"
+)
+
+func Distribute(order *dto.Order) (*http.Response, error){
+	resp := dto.Distribution{}
+	resp.Order = *order
+
+	jsonBody, err := json.Marshal(resp)
+	if err != nil {
+		log.Panic(err)
+	}
+	contentType := "application/json"
+
+	return http.Post(viper.GetString("dining_host")+"/Distribute", contentType, bytes.NewReader(jsonBody))
+}
