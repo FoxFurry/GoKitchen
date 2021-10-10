9ef1e970e6f2d60637fc8e0f121cad61dcd041b9
FoxFurry <isacartur@gmail.com>
2021-10-10 21:30:17 +0000

    Termui CUI

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.17
 require (
 	github.com/fatih/color v1.9.0
 	github.com/gin-gonic/gin v1.7.4
+	github.com/gizak/termui/v3 v3.1.0
+	github.com/jroimartin/gocui v0.5.0
 	github.com/spf13/viper v1.9.0
 )
 
@@ -21,9 +23,12 @@ require (
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mattn/go-colorable v0.1.6 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
 	github.com/mitchellh/mapstructure v1.4.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
--- internal/infrastracture/gui/gui.go
+++ internal/infrastracture/gui/gui.go
@@ -1,107 +1,257 @@
-package cui
+package gui
 
 import (
 	"context"
-	"fmt"
-	"github.com/jroimartin/gocui"
-	"log"
+	"github.com/foxfurry/go_kitchen/internal/domain/entity"
+	"github.com/foxfurry/go_kitchen/internal/domain/repository"
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+	"math/rand"
+	"strconv"
 )
+
+type ICUI interface {
+	Create() bool
+	Start(ctx context.Context, cancel context.CancelFunc)
+	AddLog(data string)
+}
+
+type CookCUI struct {
+	progress []*widgets.Gauge
+	header *widgets.Paragraph
+	catchPhrase *widgets.Paragraph
+	items *widgets.List
+}
+
+type kitchenCUI struct {
+	logs *widgets.List
+	header *widgets.Paragraph
+	tabs *widgets.TabPane
+	cooks []*CookCUI
+}
 
 type mode int
 
 const (
 	CMDMode mode = iota
-	CUIMmode
+	CUIMode
 )
 
 var (
 	AppMode = CMDMode
 )
 
-const(
-	logView = "log"
-	logChannelSize = 20
-	logX = 1
-	logY = 0.4
+const (
+	headerYStart = 0
+	headerYEnd   = headerYStart + 1
 
-	orderView = "order"
-	orderX = 0.3
-	orderY = 1 - logY
+	tabYStart = headerYEnd + 1
+	tabYEnd   = tabYStart + 1
 
-	cookView = "cook"
-	cookX = 1 - orderX
-	cookY = 1 - logY
+	cookHeaderYStart = tabYEnd + 1
+	cookHeaderYEnd = cookHeaderYStart + 1
+
+	cookCatchPhraseYStart = cookHeaderYEnd + 1
+	cookCatchPhraseYEnd = cookCatchPhraseYStart + 1
+
+	logView   = "log"
+	logYStart = tabYEnd + 2
+
+	gaugeYStart = logYStart
+	gaugeYSize = 4
+	gaugeYGap = 1
+	gaugeX = 0.7
+
+	itemsYStart = logYStart
+	itemsXStart = gaugeX + 0.01
 )
 
-var(
-	LogListener = make(chan string, logChannelSize)
-)
-
-func buildLayout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView(cookView, 0, 0, int(cookX*float32(maxX))-1, int(cookY*float32(maxY))-1); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		v.Title = "Cook status"
-	}
-	if v, err := g.SetView(orderView, int(cookX*float32(maxX)), 0, maxX-1, int(orderY*float32(maxY))-1);  err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		v.Title = "Order Queue"
-	}
-	if v, err := g.SetView(logView, 0, int(cookY*float32(maxY)), maxX-1, maxY-1); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		v.Title = "Logs"
-	}
-	return nil
-}
-
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
-}
-
-func CUIInit(ctx context.Context, cancel context.CancelFunc) {
-	g, err := gocui.NewGui(gocui.OutputNormal)
-	if err != nil {
-
-	}
-	defer g.Close()
-
-	g.SetManagerFunc(buildLayout)
-
-	if err = g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-	go listen(g, ctx)
-
-	AppMode = CUIMmode
-	if err = g.MainLoop(); err != nil {
-		if err == gocui.ErrQuit {
-			cancel()
-			AppMode = CMDMode
-		}
-		log.Panicln(err)
+func NewKitchenCUI() ICUI {
+	return &kitchenCUI{
+		logs: nil,
 	}
 }
 
-func listen(g *gocui.Gui, ctx context.Context) {
-	for{
+func (k *kitchenCUI) Create() bool{
+	cooks := repository.GetCooks()
+
+	if err := ui.Init(); err != nil {
+		return false
+	}
+
+	k.logs = buildLogWidget()
+	k.header = buildHeaderWidget()
+	k.tabs = buildTabPane(len(cooks))
+
+	for _, val := range cooks {
+		k.cooks = append(k.cooks, buildCookCUI(val))
+	}
+
+	return true
+}
+
+func (k *kitchenCUI) Start(ctx context.Context, cancel context.CancelFunc) {
+	defer ui.Close()
+	k.render()
+
+	uiEvents := ui.PollEvents()
+	for {
 		select {
+		case e := <-uiEvents:
+			switch e.ID {
+			case "q", "<C-c>":
+				AppMode = CMDMode
+				cancel()
+				return
+			case "h":
+				k.tabs.FocusLeft()
+				k.render()
+			case "l":
+				k.tabs.FocusRight()
+				k.render()
+			}
 		case <-ctx.Done():
 			return
-		case data := <-LogListener:
-			g.Update(func(g *gocui.Gui) error {
-				v, err := g.View(logView)
-				if err != nil {
-					return err
-				}
-				fmt.Fprintln(v, data)
-				return nil
-			})
 		}
 	}
 }
+
+func (k *kitchenCUI) AddLog(data string) {
+	k.logs.Rows = append(k.logs.Rows, data)
+	k.logs.ScrollBottom()
+	k.render()
+}
+
+func (k *kitchenCUI) render(){
+	ui.Clear()
+	ui.Render(k.header, k.tabs)
+
+	switch k.tabs.ActiveTabIndex {
+	case 0:
+		ui.Render(k.logs)
+	default:
+		k.cooks[k.tabs.ActiveTabIndex - 1].Render()
+	}
+}
+
+func (c *CookCUI) Render(){
+	ui.Render(c.header)
+	ui.Render(c.catchPhrase)
+	ui.Render(c.items)
+	for idx, _ := range c.progress{
+		ui.Render(c.progress[idx])
+	}
+}
+
+func buildLogWidget() *widgets.List {
+	maxX, maxY := ui.TerminalDimensions()
+	view := widgets.NewList()
+	view.Title = logView
+	view.WrapText = false
+
+	view.SetRect(0, logYStart, maxX-1, maxY-1)
+	return view
+}
+
+func buildHeaderWidget() *widgets.Paragraph {
+	maxX, _ := ui.TerminalDimensions()
+
+	header := widgets.NewParagraph()
+	header.Text = "Press q (C^c) to quit. Press h or l to switch tabs"
+	header.Border = false
+
+	header.SetRect(0, headerYStart, maxX-1, headerYEnd)
+
+	return header
+}
+
+func buildTabPane(cooksNum int) *widgets.TabPane {
+	maxX, _ := ui.TerminalDimensions()
+
+	tab := widgets.NewTabPane()
+	tab.Border=true
+
+	tabsNames := []string{"Logs"}
+
+	for idx := 0; idx < cooksNum; idx++ {
+		tabsNames = append(tabsNames, "cook #" + strconv.Itoa(idx + 1))
+	}
+
+	tab.TabNames = tabsNames
+	tab.SetRect(0, tabYStart, maxX, tabYEnd)
+
+	return tab
+}
+
+func buildCookCUI(cook entity.Cook) *CookCUI {
+	return &CookCUI{
+		progress:    buildBarsGauge(cook.Proficiency),
+		header:      buildBarHeader(cook.Name),
+		catchPhrase: buildBarCatch(cook.CatchPhrase),
+		items:       buildBarItems(cook.Proficiency),
+	}
+}
+
+func buildBarHeader(cookName string) *widgets.Paragraph {
+	maxX, _ := ui.TerminalDimensions()
+
+	header := widgets.NewParagraph()
+	header.Text = cookName
+	header.Border = false
+
+	header.SetRect(0, cookHeaderYStart, maxX-1, cookHeaderYEnd)
+
+	return header
+}
+
+func buildBarCatch(catchPhrase string)  *widgets.Paragraph{
+	maxX, _ := ui.TerminalDimensions()
+
+	header := widgets.NewParagraph()
+	header.Text = catchPhrase
+	header.Border = false
+
+	header.SetRect(0, cookCatchPhraseYStart, maxX-1, cookCatchPhraseYEnd)
+
+	return header
+}
+
+func buildBarsGauge(proficiency int) []*widgets.Gauge {
+	maxX, _ := ui.TerminalDimensions()
+
+	var result []*widgets.Gauge
+
+	for idx := 0; idx < proficiency; idx++{
+		tmp := widgets.NewGauge()
+		tmp.Title = "Item #" + strconv.Itoa(idx)
+		tmp.Percent = rand.Int()%100
+
+		tmp.BarColor = ui.ColorBlack
+		tmp.LabelStyle = ui.NewStyle(ui.ColorWhite)
+		tmpStart := gaugeYStart + gaugeYSize*idx + gaugeYGap
+		tmp.SetRect(0, tmpStart, int(float64(maxX)*gaugeX), tmpStart+gaugeYSize)
+
+		result = append(result, tmp)
+	}
+
+	return result
+}
+
+func buildBarItems(proficiency int) *widgets.List{
+	maxX, maxY := ui.TerminalDimensions()
+
+	items := widgets.NewList()
+	items.Title = "Current items: "
+	var itemsRows []string
+	for idx := 0; idx < proficiency; idx++ {
+		itemsRows = append(itemsRows, "#" + strconv.Itoa(idx) + ": n/a")
+	}
+
+	items.Rows = itemsRows
+
+	items.SetRect(int(float32(maxX)*itemsXStart), itemsYStart, maxX-1, maxY-1)
+
+	return items
+}
+
+
